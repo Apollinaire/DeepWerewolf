2ff712fe9965a90a0713776a1b740dddce9e97bb
kasimansour <kasramansouri94@gmail.com>
2017-01-27 16:29:14 +0100

    avec le calcul de distance pour Colax le fragile

--- DeepWerewolf/GameMap.cs
+++ DeepWerewolf/GameMap.cs
@@ -47,12 +47,12 @@ namespace DeepWerewolf
         {
             startTile = tuiles[abscisse, ordonnee];
         }
-        public int Distance(Tile tile1, Tile tile2)
+        public int distance(Tile tile1, Tile tile2)
         {
             int x_distance = Math.Abs(tile1.coord_x - tile2.coord_x);
             int y_distance = Math.Abs(tile1.coord_y - tile2.coord_y);
-            int distance = 0;
-            return distance;
+            int dist = Math.Max(x_distance, y_distance);
+            return dist;
 
         }
 
